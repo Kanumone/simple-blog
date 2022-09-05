844f03b34ee303906a27b697aa2f7539263ee9d2
santiago <santiago@student.21-school.ru>
2022-09-05 20:56:52 +0700

    Transfer usePagination to App component

--- src/Components/UI/pagination/Pagination.jsx
+++ src/Components/UI/pagination/Pagination.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import MyButton from "../buttons/MyButton";
 import classes from './Pagination.module.css';
-import {usePagination} from "../../../hooks/usePagination";
 
-export default function Pagination({page, setPage, totalPosts, limit}) {
-    const pagesArray = usePagination(totalPosts, limit);
+export default function Pagination({page, setPage, pagesArray}) {
     return (
         <div className={classes.page__wrapper}>
             {pagesArray.map(p =>
