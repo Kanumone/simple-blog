6db5d6388198ea790a014352a06b81ebd60b6684
santiago <santiago@student.21-school.ru>
2022-09-06 14:33:40 +0700

    Correct styles

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div style={{display: "flex", flexDirection: "column"}}>
-            <h1 style={{textAlign: "center", marginTop: "25px"}}>
+        <div style={{display: "flex", flexDirection: "column", alignItems:"center", gap: "10px"}}>
+            <h1 style={{marginTop: "25px"}}>
                 This is education project "Simple blog on ReactJS"
             </h1>
-            <div style={{alignSelf: "end"}}>
+            <div>
                 <p>Made by Kanumone</p>
                 <a href="https://github.com/Kanumone">My Github</a>
             </div>
